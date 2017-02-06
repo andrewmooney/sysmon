1376e52a62d414012891356aa4914d04fc314747
Andrew Mooney <a.mooney@uq.edu.au>
2017-02-06 10:55:44 +1000

    Adding ref to express back

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ Client.find((err, clients) => {
 	app.set('clients', clients);	
 });
 
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
